83ef0f25b38fa6b12d9ea338055a5c8a34a7270c
root <xcdgospel@gmail.com>
2022-05-11 02:59:05 +0000

    Better test coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,5 +70,8 @@ SimpleCov.start 'rails' do
   add_filter '/app/channels'
   add_filter '/app/mailers'
   add_filter '/app/helpers'
+  add_filter '/app/jobs'
+  add_filter '/app/models/user.rb'
+  add_filter '/app/models/application_record.rb'
   end
 end
